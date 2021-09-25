6c27e8cef6acbee3bb71b6ffb90b609b1581ee2c
Seyedsaman (sam) Emami <saman.emami@hotmail.com>
2021-09-25 13:28:04 +0200

    add mse

--- split_criterion/criterion.py
+++ split_criterion/criterion.py
@@ -1,4 +1,6 @@
 # %%
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import datasets as dts
 import numpy as np
 from numba import jit
 from multiprocessing import Pool
@@ -75,11 +77,71 @@ if __name__ == '__main__':
     print(bin)
 
 # %%
-from sklearn import datasets as dts
-import numpy as np
-X, y = dts.load_iris(return_X_y = True)
-np.unique(y, return_counts=True)
-# bins, maps = get_bins_maps(X, 8, 2)
+
+
+class mse():
+    def __init__(self,
+                 sq_sum_total=0.0,
+                 weighted_n_node_samples=0,
+                 n_outputs=0,
+                 sum_total=None):
+       self.sq_sum_total = sq_sum_total
+       self.weighted_n_node_samples = weighted_n_node_samples
+       self.n_outputs = n_outputs
+       self.sum_total = sum_total
+
+    def impurity(self):
+        impurity = self.sq_sum_total / self.weighted_n_node_samples
+        for k in range(self.n_outputs):
+            impurity -= (sum_total[k] / self.weighted_n_node_samples)**2.0
+
+        return impurity / self.n_outputs
+
+    def proxy_impurity_improvement(self):
+        proxy_impurity_left = 0
+        proxy_impurity_right = 0
+
+        for k in range(self.n_outputs):
+            proxy_impurity_left += self.sum_left[k] * self.sum_left[k]
+            proxy_impurity_right += self.sum_right[k] * self.sum_right[k]
+
+        return (proxy_impurity_left / self.weighted_n_left +
+                proxy_impurity_right / self.weighted_n_right)
+
+    def children_impurity(self, impurity_left, impurity_right):
+
+        sq_sum_left = 0.0
+        w = 1.0
+
+        for p in range(self.start, self.pos):
+            i = self.samples[p]
+
+            if self.sample_weight != None:
+                w = self.sample_weight[i]
+
+            for k in range(self.n_outputs):
+                y_ik = self.y[i, k]
+                sq_sum_left += w * y_ik * y_ik
+
+        sq_sum_right = self.sq_sum_total - sq_sum_left
+
+        impurity_left[0] = sq_sum_left / self.weighted_n_left
+        impurity_right[0] = sq_sum_right / self.weighted_n_right
+
+        for k in range(self.n_outputs):
+            impurity_left[0] -= (self.sum_left[k] /
+                                 self.weighted_n_left) ** 2.0
+            impurity_right[0] -= (self.sum_right[k] /
+                                  self.weighted_n_right) ** 2.0
+
+        impurity_left[0] /= self.n_outputs
+        impurity_right[0] /= self.n_outputs
+
 
 #%%
-len(bins[1])
+X, y = dts.load_boston(return_X_y=True)
+np.unique(y, return_counts=True)
+bins, maps = get_bins_maps(X, 21, 2)
+model = DecisionTreeRegressor()
+model.fit(X, y)
+model.score(X, y)
