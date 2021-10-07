3d849596bb952648b89777b8530fc38182231f79
Seyedsaman (sam) Emami <saman.emami@hotmail.com>
2021-10-07 09:13:09 +0200

    add opt for gbdtmo

---
+++ Optimization/opt_gbdtmo.py
@@ -0,0 +1,30 @@
+from sklearn.model_selection import StratifiedKFold, KFold
+from sklearn.utils.multiclass import type_of_target
+from itertools import product
+import numpy as np
+
+score = np.zeros((cv, ))
+
+def grid(estimator: 'model', X: np.array,
+         y: np.array, cv: 'int32',
+         param_grid: dict,
+         random_state: 'int32') -> "Optimize the model":
+
+  if type_of_target(y) == 'multiclass' or 'binary':
+    kfold = StratifiedKFold(n_splits=cv, shuffle=True,
+                            random_state=random_state)
+
+  else:
+    kfold = kfold(n_splits=cv, shuffle=True,
+                  random_state=random_state)
+
+  for cv_i, (train_index, test_index) in enumerate(kfold.split(X, y)):
+    x_train, x_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+    grid = [dict(zip(param_grid, v)) for v in product(*param_grid.values())]
+    for param in grid:
+      model = estimator(param)
+      model.fit(x_train, y_train)
+      model.score(x_test, y_test)
+
