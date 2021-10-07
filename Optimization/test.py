948b13ff7f58ca0cc68b6b87fa72fb2ba9530b99
Seyedsaman (sam) Emami <saman.emami@hotmail.com>
2021-10-07 14:40:36 +0200

    update test

--- Optimization/test.py
+++ Optimization/test.py
@@ -1,4 +1,5 @@
 # %%
+from itertools import product
 import sklearn.datasets as dts
 from sklearn.ensemble import GradientBoostingClassifier
 from opt_gbdtmo import grid
@@ -7,8 +8,19 @@ X, y = dts.load_iris(return_X_y=True)
 model = GradientBoostingClassifier()
 param_grid = {'learning_rate': [0.1, 0.2, 3], 'max_depth': [2, 5, 10]}
 
-grid(estimator=GradientBoostingClassifier, X=X,
-     y=y, cv=2,
-     param_grid=param_grid,
-     random_state=1)
+# grid(estimator=GradientBoostingClassifier, X=X,
+#      y=y, cv=2,
+#      param_grid=param_grid,
+#      random_state=1)
 
+
+# %%
+grid = [dict(zip(param_grid, v))
+                for v in product(*param_grid.values())]
+grid
+# %%
+import warnings
+warnings.simplefilter('ignore')
+for param in grid:
+    model = GradientBoostingClassifier(param)
+    print(model.get_params())
