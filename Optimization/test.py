a1628b9c962bf5080fd63a06376f0baab2954efc
Seyedsaman (sam) Emami <saman.emami@hotmail.com>
2021-10-07 14:27:57 +0200

    update the test

--- Optimization/test.py
+++ Optimization/test.py
@@ -7,9 +7,8 @@ X, y = dts.load_iris(return_X_y=True)
 model = GradientBoostingClassifier()
 param_grid = {'learning_rate': [0.1, 0.2, 3], 'max_depth': [2, 5, 10]}
 
-# grid(estimator=GradientBoostingClassifier, X=X,
-#      y=y, cv=2,
-#      param_grid=param_grid,
-#      random_state=1)
+grid(estimator=GradientBoostingClassifier, X=X,
+     y=y, cv=2,
+     param_grid=param_grid,
+     random_state=1)
 
-model = 
