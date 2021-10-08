4c773d15228e147109bb4b744255ab8dbcd5a90f
Seyedsaman (sam) Emami <saman.emami@hotmail.com>
2021-10-08 20:09:44 +0200

    update the h-params tuning

--- Optimization/README.md
+++ Optimization/README.md
@@ -1,3 +1,40 @@
-# Optimiation the models
+# Tuning the hyperparameters
+
+
 
 We defined an optimization class including cross-validation for each model and problem.
+
+
+## C-GB, MART, TFBT
+The optimization method for the Condensed Gradient Boosted, MART, and TFBT is the same. To use tune the hyperparameters for these models,  simply run the following method. Note that the scoring function is different for classification and regression. This method serves for these three models and binary, multi-class classification, regression, and multi-output regression.
+
+```Python
+gridsearch(X, y, model, grid,
+               scoring_functions=None,
+               pipeline=None,
+               best_scoring=True,
+               random_state=None,
+               n_cv_general=10,
+               n_cv_intrain=10)
+```
+
+## GBDT-MO
+Due to the high memory usage of the GBDT-MO model, we defined a customized gridsearch only for this model, which consumes less memory than other methods such as gridsearchCV.
+The "opt_gbdtmo" method, manage the gridsearchCV for the GBDT-MO model with the following param_grid;
+```Python
+param_grid = {"max_depth": [2, 5, 10, 20],
+              "learning_rate": [0.025, 0.05, 0.1, 0.5, 1],
+              "subsample": [0.75, 0.5, 1]}
+```
+To tune the hyperparameters of the GBDT-MO, one can use this method as follows;
+```Python
+gridsearch(X, y, cv, random_state, path, param_grid)
+```
+
+## Scoring function
+The scoring we used for different problems to find a better grid are as follows;
+<ul>
+  <li> multi-class classification: accuracy </li>
+  <li> regression: RMSE</li>
+  <li> Multioutput-Regression: r2, RMSE</li>
+</ul>
--- Optimization/opt_gbdtmo.py
+++ Optimization/opt_gbdtmo.py
@@ -1,67 +1,76 @@
-#%%
 import numpy as np
 import pandas as pd
 from itertools import product
-import sklearn.datasets as dts
-from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import StratifiedKFold, KFold
 from sklearn.utils.multiclass import type_of_target
 from gbdtmo_wrapper import classification, regression
 
 
-X, y = dts.load_iris(return_X_y=True)
+def gridsearch(X, y, cv, random_state, path, param_grid):
 
+    grid = [dict(zip(param_grid, v))
+            for v in product(*param_grid.values())]
+    score = np.zeros((cv, len(grid)))
 
-path = '/home/oem/.local/lib/python3.8/site-packages/gbdtmo/build/gbdtmo.so'
-cv = 2
-random_state = 1
+    if type_of_target(y) == 'multiclass' or 'binary':
+        kfold = StratifiedKFold(n_splits=cv, shuffle=True,
+                                random_state=random_state)
 
+    else:
+        kfold = kfold(n_splits=cv, shuffle=True,
+                      random_state=random_state)
 
+    for cv_i, (train_index, test_index) in enumerate(kfold.split(X, y)):
+        x_train, x_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
-param_grid = {"max_depth": [2, 5, 10, 20],
-              "learning_rate": [0.025, 0.05, 0.1, 0.5, 1],
-              "subsample": [0.75, 0.5, 1]}
+        for cv_grid in range(len(grid)):
+            learning_rate = (list(grid[cv_grid].values())[1])
+            max_depth = (list(grid[cv_grid].values())[0])
+            subsample = (list(grid[cv_grid].values())[2])
 
-grid = [dict(zip(param_grid, v))
-        for v in product(*param_grid.values())]
-score = np.zeros((cv, len(grid)))
+            if type_of_target(y) == 'multiclass' or 'binary':
 
-if type_of_target(y) == 'multiclass' or 'binary':
-    kfold = StratifiedKFold(n_splits=cv, shuffle=True,
-                            random_state=random_state)
+                model = classification(max_depth=max_depth,
+                                       learning_rate=learning_rate,
+                                       random_state=random_state,
+                                       num_boosters=100,
+                                       lib=path,
+                                       subsample=subsample,
+                                       verbose=False,
+                                       num_eval=0
+                                       )
 
-else:
-    kfold = kfold(n_splits=cv, shuffle=True,
-                  random_state=random_state)
+            else:
+                model = regression(max_depth=max_depth,
+                                   learning_rate=learning_rate,
+                                   random_state=random_state,
+                                   num_boosters=100,
+                                   lib=path,
+                                   subsample=subsample,
+                                   verbose=False,
+                                   num_eval=0
+                                   )
 
-for cv_i, (train_index, test_index) in enumerate(kfold.split(X, y)):
-    x_train, x_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+            model.fit(x_train, y_train)
+            score[cv_i, cv_grid] = model.score(x_test, y_test)
+            print('*', end='')
 
-    for cv_grid in range(len(grid)):
-        learning_rate = (list(grid[cv_grid].values())[1])
-        max_depth = (list(grid[cv_grid].values())[0])
-        subsample = (list(grid[cv_grid].values())[2])
+    cv_result = pd.DataFrame(score)
+    score = np.mean(score, axis=0)
+    score_std = np.std(cv_result, axis=0)
+    best_score = np.amax(score) if type_of_target(
+        y) == 'multiclass' or 'binary' else np.amin(score)
+    score_std = np.amax(score_std) if type_of_target(
+        y) == 'multiclass' or 'binary' else np.amin(score_std)
+    best_params = []
+    if np.where(score == best_score)[0].shape[0] > 1:
+        for i, j in enumerate(np.where(score == best_score)[0]):
+            best_params.append(grid[np.where(score == best_score)[0][i]])
+    else:
+        best_params = grid[np.where(score == best_score)[0][0]]
 
-        model = classification(max_depth=max_depth,
-                               learning_rate=learning_rate,
-                               random_state=random_state,
-                               num_boosters=100,
-                               lib=path,
-                               subsample=subsample,
-                               verbose=False,
-                               num_eval=0
-                               )
-
-        model.fit(x_train, y_train)
-        score[cv_i, cv_grid] = model.score(x_test, y_test)
-
-
-
-cv_result = pd.DataFrame(score)
-score = np.mean(score, axis=0)
-best_param = grid[np.where(score == np.amax(score))[0][0]]
-best_score = np.amax(score)
-#%%
-score
-best_param
+    result = {}
+    result['best_score'] = best_score
+    result['score_std'] = score_std
+    pd.Series(result).to_csv('result.csv')
