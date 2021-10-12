200c3bfb45a6d0f9b9f64a2ccb9a104a9a23a1c5
Seyedsaman (sam) Emami <54855771+samanemami@users.noreply.github.com>
2021-10-12 19:55:29 +0200

    Update plot_mprofile.py

--- Memory Experiments/plot_mprofile.py
+++ Memory Experiments/plot_mprofile.py
@@ -36,20 +36,6 @@ def plot_mprofile(path: 'path to mprofile_.da'):
     Len.append(dataframe(i).shape[0])
   axis = np.min(Len)
 
-  plt.figure(figsize=(6, 5))
-  for i, j in enumerate(files()):
-    plt.subplot(2, 2, i+1)
-    plt.plot(np.arange(start=0, stop=dataframe(i)[
-        :axis].shape[0]/10, step=0.1), dataframe(i)[:axis], label=j[:4], linewidth=2)
-    plt.title('Recorded memory - ' + j[:4])
-    plt.ylabel('Memory used (in MiB)')
-    plt.xlabel('time (in secons)')
-    plt.legend()
-    plt.grid(True)
-  plt.tight_layout()
-  plt.savefig('subplot.jpg', dpi=700)
-  plt.close('all')
-
   plt.figure(figsize=(8, 6))
   for i, j in enumerate(files()):
     plt.plot(np.arange(start=0, stop=dataframe(i)[:axis].shape[0]/10, step=0.1), dataframe(
