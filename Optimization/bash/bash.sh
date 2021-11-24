90350ecb1cd1a21ccc07bbbf532ffca7d420b645
Seyedsaman (sam) Emami <54855771+samanemami@users.noreply.github.com>
2021-11-24 21:53:17 +0100

    Update bash.sh

--- Optimization/bash/bash.sh
+++ Optimization/bash/bash.sh
@@ -2,11 +2,11 @@
 
 module load python/#version
 
-for lr in {0.025, 0.05, 0.1, 0.5, 1}
+for lr in 0.025 0.05 0.1 0.5 1
 do
-    for dp in {2, 5, 10, 20}
+    for dp in 2 5 10 20
     do
-        python opt.py $lr $dp train1 seed
-        python opt.py $lr $dp train2
+        python iris.py $lr $dp train1 1
+        python iris.py $lr $dp train2 1
     done
 done
