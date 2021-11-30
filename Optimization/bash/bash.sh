add73b6a6ab7d9ca51c8a178ec6bec4936801ff6
Seyedsaman (sam) Emami <54855771+samanemami@users.noreply.github.com>
2021-11-30 13:15:41 +0100

    Update bash.sh

--- Optimization/bash/bash.sh
+++ Optimization/bash/bash.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-module load python/#version
+line="----------------"
+
+echo $line
+echo "starting at: $(date)"; echo $line
+
 
 for lr in 0.025 0.05 0.1 0.5 1
 do
     for dp in 2 5 10 20
     do
-        python iris.py $lr $dp train1 1
-        python iris.py $lr $dp train2 1
+        python3 opt.py $lr $dp train1 1
+        python3 opt.py $lr $dp train2 1
     done
 done
+
+echo "starting at: $(date)"; echo $line
+echo "UP Time"; uptime; echo $line
+echo "memory"; free; echo $line
+echo "Finishing at $(date)"; echo $line
