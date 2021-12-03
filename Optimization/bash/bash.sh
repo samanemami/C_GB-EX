873cef2555303f7296ac597df85a17e964b208ec
Seyedsaman (sam) Emami <54855771+samanemami@users.noreply.github.com>
2021-12-03 10:32:58 +0100

    Update bash.sh

--- Optimization/bash/bash.sh
+++ Optimization/bash/bash.sh
@@ -11,9 +11,10 @@ for lr in 0.025 0.05 0.1 0.5 1
 do
     for dp in 2 5 10 20
     do
-        echo "iter:" $n;
+        echo "iter:" $n; echo $line
         sleep $n
         ((n=n+1))
+        echo "`ps aux --sort -rss`"; echo $line
         
         python3 opt.py $lr $dp train1 1
         python3 opt.py $lr $dp train2 1
@@ -31,7 +32,6 @@ do
     done
 done
 
-echo "starting at: $(date)"; echo $line
 echo "UP Time"; uptime; echo $line
 echo "memory"; free; echo $line
 echo "Finishing at $(date)"; echo $line
