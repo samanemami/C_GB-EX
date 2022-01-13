885e4706047c012dc9f35516f2bb0910538ad9c0
Seyedsaman (sam) Emami <54855771+samanemami@users.noreply.github.com>
2022-01-13 16:17:55 +0100

    Update opt.py

--- Optimization/bash/opt.py
+++ Optimization/bash/opt.py
@@ -1,13 +1,7 @@
 import sklearn.datasets as dts
 from gridsearch import gridsearchcv
 
-
 X, y = dts.load_digits(return_X_y=True)
 
-
 if __name__ == '__main__':
-    gridsearchcv(X=X,
-                 y=y,
-                 num=100,
-                 random_state=1,
-                 loss=b"ce")
+    gridsearchcv(X, y, 20, 100, b"ce", 1, False)
