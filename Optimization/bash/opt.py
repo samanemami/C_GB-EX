b09183e98239fe411886b7d813a02a9091094cbf
Seyedsaman (Sam) Emami <54855771+samanemami@users.noreply.github.com>
2022-01-23 11:20:07 +0100

    Update opt.py

--- Optimization/bash/opt.py
+++ Optimization/bash/opt.py
@@ -4,4 +4,10 @@ from gridsearch import gridsearchcv
 X, y = dts.load_digits(return_X_y=True)
 
 if __name__ == '__main__':
-    gridsearchcv(X, y, 20, 100, b"ce", 1, False)
+    gridsearchcv(X=X,
+                 y=y,
+                 num_train=20,
+                 num_test=100,
+                 loss=b"ce",
+                 random_state=1,
+                 verbose=True)
