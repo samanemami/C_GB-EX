09b99a2d7de45fae1023042f739bbf508669a4b7
Seyedsaman (Sam) Emami <54855771+samanemami@users.noreply.github.com>
2022-02-07 10:52:45 +0100

    Update ensemble_size.py

--- Ensemble_size/ensemble_size.py
+++ Ensemble_size/ensemble_size.py
@@ -1,1 +1,100 @@
+import numpy as np
+import sklearn.datasets as dt
+from sklearn.model_selection import train_test_split
+from Scikit_CGB import C_GradientBoostingClassifier
+from gbdtmo import GBDTMulti, load_lib, create_graph
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.preprocessing import LabelEncoder, Normalizer
+
+X, y = dt.load_iris(return_X_y=True)
+n_class = len(np.unique(y))
+
+max_depth = 5
+random_state = 1
+path = '/lustre/home/samanema/.local/lib/python3.6/site-packages/gbdtmo/build/gbdtmo.so'
+lib = load_lib(path)
+
+
+x_train, x_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=random_state)
+
+
+cgb = C_GradientBoostingClassifier(max_depth=max_depth,
+                                   subsample=1,
+                                   max_features="sqrt",
+                                   learning_rate=0.1,
+                                   random_state=random_state,
+                                   criterion="mse",
+                                   loss="deviance",
+                                   n_estimators=1)
+
+
+cgb.fit(x_train, y_train)
+print('C-GB Leaves')
+print(sum(tree.tree_.n_leaves for tree in cgb.estimators_.reshape(-1)))
+
+
+mart = GradientBoostingClassifier(max_depth=max_depth,
+                                  subsample=1,
+                                  max_features="sqrt",
+                                  learning_rate=0.1,
+                                  random_state=random_state,
+                                  criterion="mse",
+                                  n_estimators=1)
+
+mart.fit(x_train, y_train)
+print('MART Leaves')
+print(sum(tree.tree_.n_leaves for tree in mart.estimators_.reshape(-1)))
+
+
+params = {"max_depth": max_depth,
+          "lr": 0.1,
+          'loss': b"ce",
+          'verbose': True,
+          'seed': random_state}
+
+
+lb = LabelEncoder()
+scl = Normalizer()
+
+y_test = lb.fit_transform(y_test)
+x_train = scl.fit_transform(x_train)
+x_test = scl.fit_transform(x_test)
+y_train = lb.fit_transform(y_train)
+
+x_train, y_train = np.ascontiguousarray(
+    x_train, dtype=np.float64), y_train.astype(np.int32)
+x_test, y_test = np.ascontiguousarray(
+    x_test, dtype=np.float64), y_test.astype(np.int32)
+
+
+booster = GBDTMulti(lib, out_dim=n_class, params=params)
+booster.set_data((x_train, y_train), (x_test, y_test))
+booster.train(100)
+booster.dump(b"iris.txt")
+
+graph = create_graph("iris.txt", 1, [0, n_class-1])
+graph.render("Tree", format='pdf')
+
+tree_index = 100
+nodes = []
+dumped_model = "iris.txt"
+gbdtmo_nodes = []
+for i in range(tree_index):
+    with open(dumped_model, "r") as f:
+        tree = f.read().split("Booster")
+        tree.pop(0)
+        tree = tree[i]
+        tree = tree.split("\n")[1:]
+        for line in tree:
+            line = line.strip().split(",")
+            if len(line) <= 1:
+                continue
+            node = int(line.pop(0))
+            if node > 0:
+                nodes.append(node)
+    gbdtmo_nodes.append(nodes)
+
+print('GBDT-MO Leaves')
+print(len(gbdtmo_nodes[tree_index-1]))
 
