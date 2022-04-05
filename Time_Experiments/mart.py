90dda1f0e2a16d0e408aeae2d444b4112d68d39e
Seyedsaman (Sam) Emami <54855771+samanemami@users.noreply.github.com>
2022-04-05 17:51:27 +0200

    Create mart.py

---
+++ Time_Experiments/mart.py
@@ -0,0 +1,105 @@
+from sklearn.ensemble._gb import BaseGradientBoosting
+from sklearn.ensemble import GradientBoostingClassifier
+
+
+class mart(BaseGradientBoosting):
+    def __init__(self,
+                 n_estimators=100,
+                 learning_rate=0.1,
+                 loss="deviance",
+                 criterion="squared_error",
+                 min_samples_split=2,
+                 min_samples_leaf=1,
+                 min_weight_fraction_leaf=0.0,
+                 subsample=1.0,
+                 max_features=None,
+                 max_depth=5,
+                 min_impurity_decrease=0.0,
+                 ccp_alpha=0.0,
+                 alpha=0.9,
+                 verbose=0,
+                 max_leaf_nodes=None,
+                 warm_start=False,
+                 validation_fraction=0.1,
+                 n_iter_no_change=None,
+                 tol=0.0001,
+                 init=None,
+                 random_state=None):
+
+        super().__init__(n_estimators=n_estimators,
+                         learning_rate=learning_rate,
+                         loss=loss,
+                         criterion=criterion,
+                         min_samples_split=min_samples_split,
+                         min_samples_leaf=min_samples_leaf,
+                         min_weight_fraction_leaf=min_weight_fraction_leaf,
+                         subsample=subsample,
+                         max_features=max_features,
+                         max_depth=max_depth,
+                         min_impurity_decrease=min_impurity_decrease,
+                         ccp_alpha=ccp_alpha,
+                         alpha=alpha,
+                         verbose=verbose,
+                         max_leaf_nodes=max_leaf_nodes,
+                         warm_start=warm_start,
+                         validation_fraction=validation_fraction,
+                         n_iter_no_change=n_iter_no_change,
+                         tol=tol,
+                         init=init,
+                         random_state=random_state)
+
+    def _raw_predict(self, X):
+        import numpy as np
+        raw_predictions = self._raw_predict_init(X)
+        for i in range(self.n_estimators):
+            for k in range(self.loss_.K):
+                tree = self.estimators_[i, k].tree_
+                raw_predictions[:, k] += np.squeeze(self.learning_rate *
+                                                    tree.predict(X))
+        return raw_predictions
+
+
+class GradientBoostingClassifier(GradientBoostingClassifier, mart):
+    def __init__(self,
+                 *,
+                 loss='deviance',
+                 learning_rate=0.1,
+                 n_estimators=100,
+                 subsample=1.0,
+                 criterion='squared_error',
+                 min_samples_split=2,
+                 min_samples_leaf=1,
+                 min_weight_fraction_leaf=0.,
+                 max_depth=3,
+                 min_impurity_decrease=0.,
+                 init=None,
+                 random_state=None,
+                 max_features=None,
+                 verbose=0,
+                 max_leaf_nodes=None,
+                 warm_start=False,
+                 validation_fraction=0.1,
+                 n_iter_no_change=None,
+                 tol=1e-4,
+                 ccp_alpha=0.0):
+
+        super().__init__(loss=loss,
+                         learning_rate=learning_rate,
+                         n_estimators=n_estimators,
+                         criterion=criterion,
+                         min_samples_split=min_samples_split,
+                         min_samples_leaf=min_samples_leaf,
+                         min_weight_fraction_leaf=min_weight_fraction_leaf,
+                         max_depth=max_depth,
+                         init=init,
+                         subsample=subsample,
+                         max_features=max_features,
+                         random_state=random_state,
+                         verbose=verbose,
+                         max_leaf_nodes=max_leaf_nodes,
+                         min_impurity_decrease=min_impurity_decrease,
+                         warm_start=warm_start,
+                         validation_fraction=validation_fraction,
+                         n_iter_no_change=n_iter_no_change,
+                         tol=tol,
+                         ccp_alpha=ccp_alpha)
