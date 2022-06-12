58ffed3236eb45a8b88475b83831851c3d3ffc62
Seyedsaman (Sam) Emami <54855771+samanemami@users.noreply.github.com>
2022-06-12 10:20:35 +0200

    Update loss_curve.py

--- accuracy_error/loss_curve.py
+++ accuracy_error/loss_curve.py
@@ -1,36 +1,35 @@
-import warnings
-import numpy as np
-from cgb import cgb_clf
-import sklearn.datasets as dts
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
+from cgb import cgb_clf
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+
 
 warnings.simplefilter('ignore')
 
 
-def loss_curve(max_depth=2, n_classes=3):
+def loss_curve(max_depth=2):
 
     # Defining the toy dataset
-    X, y = dts.make_classification(n_features=2,
-                                   n_redundant=0,
-                                   n_informative=2,
-                                   random_state=2,
-                                   n_clusters_per_class=1,
-                                   n_classes=n_classes,
-                                   n_samples=1040,
-                                   flip_y=0.15)
+    waveform = np.loadtxt('waveform.data', delimiter=',')
+    X = waveform[:, :-1]
+    y = waveform[:, -1]
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1)
 
     #  Training the proposed condensed method
     c_gb = cgb_clf(max_depth=max_depth,
-                  subsample=0.75,
-                  max_features="sqrt",
-                  learning_rate=0.1,
-                  random_state=random_state,
-                  n_estimators=T,
-                  criterion='100')
+                   subsample=0.75,
+                   max_features="sqrt",
+                   learning_rate=0.1,
+                   random_state=1,
+                   criterion="squared_error",
+                   n_estimators=100)
 
-    c_gb.fit(X, y)
+    c_gb.fit(x_train, y_train)
 
     # training the Mart
     mart = GradientBoostingClassifier(max_depth=max_depth,
@@ -40,7 +39,7 @@ def loss_curve(max_depth=2, n_classes=3):
                                       random_state=1,
                                       criterion="squared_error",
                                       n_estimators=100)
-    mart.fit(X, y)
+    mart.fit(x_train, y_train)
 
     # Plotting the training loss curve
     plt.plot(mart.train_score_, color='r',
@@ -48,7 +47,7 @@ def loss_curve(max_depth=2, n_classes=3):
     plt.plot(c_gb.train_score_, color='b', alpha=0.4, label='C-GB')
     plt.title('Max depth=' + str(max_depth))
     plt.xlabel('Boosting iteration')
-    plt.ylabel('Training loss')
+    plt.ylabel('Training error')
     plt.grid(visible=True, alpha=0.3)
 
 
@@ -61,6 +60,4 @@ if __name__ == '__main__':
     plt.legend(loc='upper center', bbox_to_anchor=(-.15, -0.13),
                fancybox=False, shadow=False, ncol=2)
     plt.subplots_adjust(hspace=0.6, wspace=0.2)
-    # plt.tight_layout()
-    plt.savefig('loss_curve.jpg', dpi=700)
-    plt.savefig('loss_curve.eps')
+    plt.savefig('loss_curve.jpg', dpi=1000)
