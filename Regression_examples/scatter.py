aca90d51ac7a73a82141f97eb5a32295e63b5fe6
Seyedsaman (Sam) Emami <54855771+samanemami@users.noreply.github.com>
2022-06-12 21:04:27 +0200

    Update scatter.py

--- Regression_examples/scatter.py
+++ Regression_examples/scatter.py
@@ -1,4 +1,3 @@
-# %%
 from sklearn.ensemble import GradientBoostingRegressor
 from cgb import cgb_reg
 import seaborn as sns
@@ -8,8 +7,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import warnings
-import sys
-sys.path.append(r'D:\Academic\Ph.D\Programming\Py\PhD Thesis\CGB\C-GB')
+
 
 
 warnings.simplefilter('ignore')
