049e160e8bc2f67b8b36ce702a4446540b3d0cc2
Seyedsaman (Sam) Emami <54855771+samanemami@users.noreply.github.com>
2022-06-26 20:15:34 +0200

    Update GB_tree.py

--- Tree/GB_tree.py
+++ Tree/GB_tree.py
@@ -1,12 +1,11 @@
-import warnings
-import numpy as np
-from cgb import cgb_clf
-import sklearn.datasets as dts
-import matplotlib.pyplot as plt
-from sklearn.tree import plot_tree
-from scipy.interpolate import make_interp_spline
 from sklearn.ensemble import GradientBoostingClassifier
-
+from scipy.special import logsumexp
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+import sklearn.datasets as dts
+from cgb import cgb_clf
+import numpy as np
+import warnings
 
 
 warnings.simplefilter("ignore")
@@ -22,32 +21,43 @@ X, y = dts.make_classification(n_features=2,
                                n_samples=100,
                                flip_y=0.15)
 
-
 def plot(tree, axs):
 
     plot_tree(tree, filled=True, rounded=True,
               precision=2, ax=axs, label="root")
 
 
-def boundaries(X, model, tree, class_, axs):
+def boundaries(X, y, model, tree, title, axs):
 
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
                          np.arange(y_min, y_max, 0.1))
 
-    if model.estimators_.shape[1] > 2:
-        tree_ = model.estimators_[tree][class_]
-        Z = tree_.predict(np.c_[xx.ravel(), yy.ravel()])
-        Z = Z.reshape(xx.shape)
+    n_class = len(np.unique(y))
+    learning_rate = model.get_params()['learning_rate']
 
+    if model.estimators_.shape[1] > 2:
+
+        pred = np.zeros((xx.ravel().shape[0], n_class))
+        for i in range(n_class):
+            tree_ = model.estimators_[tree][i]
+            pred_ = tree_.predict(np.c_[xx.ravel(), yy.ravel()])
+            pred[:, i] = pred_ * learning_rate
     else:
         tree_ = model.estimators_[tree][0]
-        Z = tree_.predict(np.c_[xx.ravel(), yy.ravel()])
-        Z = Z[:, class_].reshape(xx.shape)
+        pred_ = tree_.predict(np.c_[xx.ravel(), yy.ravel()])
+        pred = pred_ * learning_rate
+
+    proba = np.nan_to_num(
+        np.exp(pred - (logsumexp(pred, axis=1)[:, np.newaxis])))
+    Z = np.argmax(proba, axis=1)
+    Z = Z.reshape(xx.shape)
+
+    axs.scatter(X[:, 0], X[:, 1], c=y, s=20, edgecolor='k')
 
     axs.contourf(xx, yy, Z, alpha=0.4)
-    axs.set_title("class " + str(class_))
+    axs.set_title(title)
     plt.gca().set_xlim(xx.min(), xx.max())
     plt.gca().set_ylim(yy.min(), yy.max())
     axs.grid(True)
@@ -78,8 +88,7 @@ def model(random_state=1):
 
     fig1, axs1 = plt.subplots(1, 1, figsize=(10, 3), facecolor="w")
     fig2, axs2 = plt.subplots(1, 2, figsize=(30, 7), facecolor="w")
-    fig3, axs3 = plt.subplots(1, 3, figsize=(30, 7), facecolor="w")
-    fig4, axs4 = plt.subplots(1, 3, figsize=(30, 7), facecolor="w")
+    fig3, axs3 = plt.subplots(1, 2, figsize=(30, 7), facecolor="w")
 
     for i in range(1, 3):
         exec(f'fig{i}.subplots_adjust(hspace=-0.5, wspace=-0.15)')
@@ -87,23 +96,16 @@ def model(random_state=1):
     plot(tree_cgb, axs=axs1)
 
     for i in range(n_classes):
-
         tree_gb = gb.estimators_[0][i]
-        boundaries(X=X, model=cgb, tree=0, class_=i, axs=axs3[i])
-        boundaries(X=X, model=gb, tree=0, class_=i, axs=axs4[i])
         j = i
         if j < 2:
             plot(tree_gb, axs2[j])
         j += 1
 
-    fig3.suptitle("CGB Boundaries")
-    fig4.suptitle("GB Boundaries")
-
-    fig1.savefig("C_GB_Tree.jpg", dpi=1000)
-    fig2.savefig("GB_Tree.jpg", dpi=1000)
-    fig3.savefig("CGB_boundaries.jpg", dpi=1000)
-    fig4.savefig("GB_boundaries.jpg", dpi=1000)
-
+    boundaries(X=X, y=y, model=cgb, tree=0, title='C-GB', axs=axs3[0])
+    boundaries(X=X, y=y, model=gb, tree=0, title='GB', axs=axs3[1])
+    
+    fig3.suptitle("Decision Boundaries for the first tree")
 
 if __name__ == "__main__":
     model()
